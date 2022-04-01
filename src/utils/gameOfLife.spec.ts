76b38a6575a3e437c88a94bb05626c0793a92460
mish3n <mish3n@gmail.com>
2022-04-01 19:05:11 +0100

    implemented getNextCycle with neighbour count

--- src/utils/gameOfLife.spec.ts
+++ src/utils/gameOfLife.spec.ts
@@ -1,6 +1,22 @@
-import { getGridPositionByIndex, getIndexByGridPosition } from "./gameOfLife";
+import { getGridPositionByIndex, getIndexByGridPosition, getNextCycle } from "./gameOfLife";
 
 describe("Game Of Life", () => {
+    describe("getNextCycle", () => {
+        it.each([
+            [ "1.", [], 0, 0, []],
+            [ "2.", [1], 1, 1, [0]],
+            [ "3.", [1, 1], 2, 1, [1, 1]],
+            [ "4.", [1, 1], 1, 2, [1, 1]],
+            [ "5.", [1, 0, 1], 1, 3, [0, 2, 0]],
+            [ "6.", [1, 1, 1, 1, 1, 1, 1, 1, 1], 3, 3, [3, 5, 3, 5, 8, 5, 3, 5, 3]],
+            [ "7.", [0, 0, 0, 0, 1, 0, 0, 0, 0], 3, 3, [1, 1, 1, 1, 0, 1, 1, 1, 1]],
+            [ "8.", [0, 1, 0, 1, 1, 0, 0, 0, 0], 3, 3, [3, 2, 2, 2, 2, 2, 2, 2, 1]],
+            [ "9.", [0, 0, 0, 0, 0, 0, 0, 0, 1], 3, 3, [0, 0, 0, 0, 1, 1, 0, 1, 0]],
+        ])("%s. should calc neighbours correctly", (_, grid, rows, cols, expected) => {
+            expect(getNextCycle(grid, rows, cols)).toStrictEqual(expected);
+        });
+    });
+
     describe("getIndexByGridPosition", () => {
         it.each([
             ["1.", 10, 0, 0, 0],
--- src/utils/gameOfLife.ts
+++ src/utils/gameOfLife.ts
@@ -9,10 +9,33 @@ const RELATIVE_NEIGHBOUR_POSITIONS = [
     [1, 1],
 ];
 
-export const getNextCycle = (grid: number[], rows: number, cols: number) => {}
+export const getNextCycle = (grid: number[], rows: number, cols: number) => {
+    const nextCycle = new Array(grid.length).fill(0);
+
+    grid.forEach((_, ind) => {
+        const [row, col] = getGridPositionByIndex(cols)(ind);
+        RELATIVE_NEIGHBOUR_POSITIONS.forEach(([relRow, relCol]) => {
+            if (isInBounds(rows, cols)(row + relRow, col + relCol)) {
+                const neighbourInd = getIndexByGridPosition(cols)(row + relRow, col + relCol);
+                nextCycle[ind] += grid[neighbourInd];
+            }
+        });
+    });
+
+    return nextCycle;
+}
 
 export const getGridPositionByIndex = (cols: number) =>
     (index: number) => [Math.floor(index / cols), index % cols];
 
 export const getIndexByGridPosition = (cols: number) =>
     (row: number, col: number) => (row * cols) + col;
+
+const isInBounds = (rows: number, cols: number) =>
+    (row: number, col: number) => {
+        return (
+            row >= 0 && col >= 0 &&
+            row < rows && col < cols
+        );
+    }
+
