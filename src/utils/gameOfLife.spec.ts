2658aeaed9351e7a8d995dd984931b2193320825
mish3n <mish3n@gmail.com>
2022-04-01 18:44:45 +0100

    added getGridPositionByIndex helper fn

--- src/utils/gameOfLife.spec.ts
+++ src/utils/gameOfLife.spec.ts
@@ -1,4 +1,4 @@
-import { getIndexByGridPosition } from "./gameOfLife";
+import { getGridPositionByIndex, getIndexByGridPosition } from "./gameOfLife";
 
 describe("Game Of Life", () => {
     describe("getIndexByGridPosition", () => {
@@ -8,6 +8,18 @@ describe("Game Of Life", () => {
             ["3.", 5, 5, 1, 26],
         ])("%s should calc ind correctly", (_, cols, row, col, expected) => {
             expect(getIndexByGridPosition(cols)(row, col)).toStrictEqual(expected);
+        });
+    });
+
+    describe("getGridPositionByIndex", () => {
+        it.each([
+            ["1.", 10, 0, [0, 0]],
+            ["2.", 5, 7, [1, 2]],
+            ["3.", 3, 13, [4, 1]],
+            ["4.", 100, 13, [0, 13]],
+            ["5.", 100, 100, [1, 0]],
+        ])("%s should calc ind correctly", (_, cols, index, expected) => {
+            expect(getGridPositionByIndex(cols)(index)).toStrictEqual(expected);
         });
     });
 });
--- src/utils/gameOfLife.ts
+++ src/utils/gameOfLife.ts
@@ -1,4 +1,18 @@
+const RELATIVE_NEIGHBOUR_POSITIONS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1],
+];
+
 export const getNextCycle = (grid: number[], rows: number, cols: number) => {}
+
+export const getGridPositionByIndex = (cols: number) =>
+    (index: number) => [Math.floor(index / cols), index % cols];
 
 export const getIndexByGridPosition = (cols: number) =>
     (row: number, col: number) => (row * cols) + col;
